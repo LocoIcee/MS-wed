f16e206b7e2526194697b47cc3ac1e0fbb24a96a
LocoIcee <62861210+LocoIcee@users.noreply.github.com>
2026-02-19 22:58:54 -0700

    add location

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import {MapPinned} from 'lucide-react';
+
 export default function Home() {
   return (
     <div className="relative min-h-screen w-full bg-[url('/imgs/home.JPG')] bg-cover bg-center text-white">
@@ -10,7 +12,11 @@ export default function Home() {
           <p className="page-subtitle mt-3 text-base uppercase tracking-[0.35em] text-white/90 drop-shadow-[0_4px_8px_rgba(0,0,0,0.45)] lg:text-4xl">
             Are Getting Married
           </p>
-          <p className="page-lede mt-2 text-sm uppercase tracking-[0.3em] text-white/85 drop-shadow-[0_4px_8px_rgba(0,0,0,0.45)] lg:text-3xl">
+          
+          <p className="page-lede mt-2 flex items-center justify-center gap-2 text-sm uppercase tracking-[0.3em] text-white/85 drop-shadow-[0_4px_8px_rgba(0,0,0,0.45)] lg:text-xl">
+            <MapPinned className="h-5 w-5 shrink-0 lg:h-6 lg:w-6" /> L'Olivo Cicchetti & Lounge
+          </p>
+          <p className="page-lede mt-2 text-sm uppercase tracking-[0.3em] text-white/85 drop-shadow-[0_4px_8px_rgba(0,0,0,0.45)] lg:text-xl">
             July 23, 2026 at 5:00pm
           </p>
         </div>
